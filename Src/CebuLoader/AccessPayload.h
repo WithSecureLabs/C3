0c42bdcc8b1381663836be9e0face510bc6880bb
Pawel Kurowski <pawel.kurowski@mwrinfosecurity.com>
2020-01-03 10:16:00 +0100

    Add resource generator that will embed NodeRelayDll

---
+++ Src/CebuLoader/AccessPayload.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "EmbeddedResource.h"
+#ifndef EMBEDDED_DLL_PAYLOAD
+#error Unknown resource guid.
+#endif // !EMBEDDED_DLL_PAYLOAD
+
+// Payload form [16 byte guid][1 byte terminator 0xff][4 byte size][body]
+
+static bool ByteCompare(char* a, char* b, size_t size)
+{
+	for (size_t i = 0; i < size; ++i)
+		if (a[i] != b[i])
+			return false;
+
+	return true;
+}
+
+static char* FindStartOfResource(void* startofImage, size_t sizeOfImage)
+{
+	if (sizeOfImage >= 21)
+		for (char* p = (char*) startofImage; p < (char*) startofImage + sizeOfImage - 21; ++p)
+			if (ByteCompare(p, EMBEDDED_DLL_PAYLOAD, 16) && p[16] == '\xff')
+				return p;
+
+	return NULL;
+}
+
+static size_t GetPayloadSize(void* startOfResource)
+{
+	return *((int32_t*)((char*) startOfResource + 17)); // 16 bytes guid, 0xff terminator
+}
+
+static char* GetPayload(void* startOfResource)
+{
+	return (char*) startOfResource + 21; //16 bytes guid, 0xff terminator, four bytes size.
+}
---
+++ Src/Tools/ResourceGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace ResourceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2 || !File.Exists(args[0]) || !Directory.Exists(args[1]))
+            {
+                System.Console.WriteLine("ResourceGenerator require existing input file and output directory.");
+                return;
+            }
+
+            var guid = Guid.NewGuid().ToByteArray();
+            using (var sw = new StreamWriter(Path.Join(args[1], "EmbeddedResource.h"), false) { AutoFlush = true })
+            {
+                sw.Write("#define EMBEDDED_DLL_PAYLOAD \"");
+                foreach (var oneByte in guid)
+                    sw.Write(String.Format("\\x{0:x2}", oneByte));
+                sw.Write("\"\n");
+            }
+
+
+            using (var bw = new BinaryWriter(File.Open(Path.Join(args[1], "EmbeddedResource.dat"), FileMode.Create)))
+            {
+                var data = File.ReadAllBytes(args[0]);
+                bw.Write(guid);
+                bw.Write((byte)0xff);
+                bw.Write(BitConverter.GetBytes(data.Length));
+                bw.Write(data);
+            }
+        }
+    }
+}
