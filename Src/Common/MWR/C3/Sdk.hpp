031f3edf7e60c0354a19675a25cff1e285ebbf63
Grzegorz Rychlik <Grzegorz.Rychlik@f-secure.com>
2020-03-05 10:02:15 +0100

    Fix rwdi lib linking, restore default inlining

--- Src/Common/MWR/C3/Sdk.hpp
+++ Src/Common/MWR/C3/Sdk.hpp
@@ -8,12 +8,16 @@
 #ifdef _WIN64
 #	ifdef _DEBUG
 #		pragma comment(lib, "../Bin/Core_d64.lib")
+#	elif defined(C3_RWDI)
+#		pragma comment(lib, "../Bin/Core_rwdi64.lib")
 #	else
 #		pragma comment(lib, "../Bin/Core_r64.lib")
 #	endif
 #else
 #	ifdef _DEBUG
 #		pragma comment(lib, "../Bin/Core_d86.lib")
+#	elif defined(C3_RWDI)
+#		pragma comment(lib, "../Bin/Core_rwdi86.lib")
 #	else
 #		pragma comment(lib, "../Bin/Core_r86.lib")
 #	endif
