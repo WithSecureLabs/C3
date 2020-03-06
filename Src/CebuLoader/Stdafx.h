0065374c98cbdc8c3560df1bfac0df04f166464c
Grzegorz Rychlik <Grzegorz.Rychlik@f-secure.com>
2020-03-06 16:37:14 +0100

    Fix post-merge problems for clang-cl

--- Src/CebuLoader/Stdafx.h
+++ Src/CebuLoader/Stdafx.h
@@ -16,14 +16,7 @@
 #include "UnexportedWinApi.h"
 #include "LoadPe.h"
 
-// C3 includes
-//#include "Common/FSecure/CppTools/ByteConverter.h"
-
 // Windows includes
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-
-#include <Ehdata.h>																										//< For VEH constants: EH_MAGIC_NUMBER1, EH_PURE_MAGIC_NUMBER1, EH_EXCEPTION_NUMBER.
-#include <intrin.h>																										//< For _ReturnAddress().
-#include <winnt.h>
