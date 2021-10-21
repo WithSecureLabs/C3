c1c8bc983e913ae8b3eeb0b21dde3a23bdeb9e07
Grzegorz Rychlik <Grzegorz.Rychlik@f-secure.com>
2021-10-21 09:51:15 +0200

    Remove setting tmp env var in StartWebController.cmd

--- StartWebController.cmd
+++ StartWebController.cmd
@@ -1,8 +1,8 @@
 @echo off
 cd WebController
 if "%1"=="" (
-	set tmp="http://localhost:52935"
-) else ( 
-	set tmp=%1
+	set c3webcontroller_url="http://localhost:52935"
+) else (
+	set c3webcontroller_url=%1
 )
-dotnet C3WebController.dll --urls %tmp%
+dotnet C3WebController.dll --urls %c3webcontroller_url%
